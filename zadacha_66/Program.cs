503ca0d633a5609d2618e18364ed8124b2edbffa
elizavetanikolaenko2017 <elizaveta.nikolaenko2017@gmail.com>
2023-03-29 21:01:20 +0300

    задача №66

---
+++ zadacha_66/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+static void ReadInt(string promt)
+{
+    Console.WriteLine(promt);
+     Convert.ToInt32(Console.ReadLine());
+}
+
+static int NewSum(int m, int n)
+{
+    if(m==n)
+    return n;
+    return n + NewSum(m,n-1);
+}
+
+static void Main(string[]args)
+{
+  Console.WriteLine(NewSum(ReadInt("Введите число m: "),ReadInt("Введите число n: "));
+}
---
+++ zadacha_68/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int ReadInt(string promt)
+{
+    Console.WriteLine(promt);
+     Convert.ToInt32(Console.ReadLine());
+}
+
+ int Recursion(int m, int n)
+{
+ if(m==0)
+ {
+   return n +1; 
+ }
+  
+  else if(n==0 && m>0)
+  {
+    return Recursion(m-1,1);
+  }
+  else
+  {
+    return Recursion(m-1,Recursion(m, n-1));
+  }
+
+}
+
+int m= ReadInt("Введите число m: ");
+int n= ReadInt("Введите число n: ");
+int AkkermanF=Recursion(m,n);
+Console.Write($"Функция Аккермана= {AkkermanF}");
